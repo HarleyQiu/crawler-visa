c92727a52895208f768d617ddf8bc1eda790bfdb
QiuLin-hao <210554114@qq.com>
2024-09-12 14:07:58 +0800

    添加创建、更新和删除应用程序API端点
    此提交引入了用于在Redis中创建、更新和删除应用程序状态记录的新API端点。它包括`CreateApplication`函数以创建新记录，`UpdateApplication`函数修改现有记录，以及`DeleteApplication`函数移除记录，每个函数都配有适当的错误处理和响应消息。此外，这些操作的路由已在应用程序的路由器中注册。

--- controller/us-status-controller.go
+++ controller/us-status-controller.go
@@ -16,6 +16,10 @@ var redisClient = config.ConfigureRedis()
 
 const keyPrefix = "application:status:"
 
+// CreateApplication 根据提供的请求数据在Redis中创建应用状态记录。
+// 它将请求正文解析为QueryUsStatus模型，将其编组为JSON，
+// 并使用由应用程序ID构造的密钥将其存储在Redis中。
+// 如果成功，它会响应一条指示成功的JSON消息；否则，它返回错误响应。
 func CreateApplication(w http.ResponseWriter, r *http.Request) {
 	queryUsStatus := &models.QueryUsStatus{}
 	utils.ParseBody(r, queryUsStatus)
@@ -48,4 +52,47 @@ func RetrieveApplication(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	utils.ResultJSON(w, result, "检索成功")
+}
+
+func UpdateApplication(w http.ResponseWriter, r *http.Request) {
+	queryUsStatus := &models.QueryUsStatus{}
+	utils.ParseBody(r, queryUsStatus)
+	exists, err := redisClient.Exists(ctx, keyPrefix+queryUsStatus.ApplicationID).Result()
+	if err != nil {
+		utils.ResultError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if exists == 0 {
+		utils.ResultError(w, "键不存在，无法更新", http.StatusBadRequest)
+		return
+	}
+	marshal, err := json.Marshal(queryUsStatus) //
+	if err != nil {
+		utils.ResultError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = redisClient.Set(ctx, keyPrefix+queryUsStatus.ApplicationID, marshal, 0).Err()
+	if err != nil {
+		utils.ResultError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	utils.ResultJSON(w, nil, "修改成功")
+}
+
+func DeleteApplication(w http.ResponseWriter, r *http.Request) {
+	appID := r.URL.Query().Get("application_id")
+	if appID == "" {
+		utils.ResultError(w, "Application ID is required", http.StatusInternalServerError)
+		return
+	}
+	result, err := redisClient.Del(ctx, keyPrefix+appID).Result()
+	if err != nil {
+		utils.ResultError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if result == 0 {
+		utils.ResultError(w, "Application not found", http.StatusNotFound)
+		return
+	}
+	utils.ResultJSON(w, nil, "删除成功")
 }
--- router/register-routers.go
+++ router/register-routers.go
@@ -11,4 +11,6 @@ var RegisterRouters = func(router *mux.Router) {
 
 	router.HandleFunc("/wuai/system/crawler_visa/cn-us/create", controller.CreateApplication).Methods("POST")
 	router.HandleFunc("/wuai/system/crawler_visa/cn-us/get", controller.RetrieveApplication).Methods("GET")
+	router.HandleFunc("/wuai/system/crawler_visa/cn-us/update", controller.UpdateApplication).Methods("PUT")
+	router.HandleFunc("/wuai/system/crawler_visa/cn-us/delete", controller.DeleteApplication).Methods("DELETE")
 }
